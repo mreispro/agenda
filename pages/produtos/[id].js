35b483baa2edce061deafdea4966468914ec278d
Marcos Proença <marcosproenca@MacBook-Pro-de-Marcos.local>
2021-06-19 18:45:42 -0300

    produtos

---
+++ pages/produtos/[id].js
@@ -0,0 +1,15 @@
+export async function getServerSideProps(context) {
+    const id = context.query.id;
+
+    return {
+        props: {
+            id: id
+        }
+    }
+}
+
+function Produtos(props) {
+    return <div>Id do produto: {props.id}</div>
+}
+
+export default Produtos;
