ab6d7c45e94b41f99c89207d9677421949a676c7
Marcos Proença <marcosproenca@MacBook-Pro-de-Marcos.local>
2021-08-18 10:20:51 -0300

    teste chat

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,19 @@
 function Home() {
+        <script type="text/javascript">
+          (function(s, z, c, h, a, t) {
+            s.webchat = s.webchat || function() {
+              (s.webchat.q = s.webchat.q || []).push(arguments);
+            };
+            t = z.createElement(c),
+              a = z.getElementsByTagName(c)[0]; t.async = 1;
+            t.src = 'https://prolumini.sz.chat/webchat/v2/webchat.js'; 
+            a.parentNode.insertBefore(t, a);
+            s.webchat('cid', '5ef63ebf915a726c837b9f18');
+            s.webchat('host', h);
+          })(window, document, 'script', 'https://prolumini.sz.chat');
+        </script>
+
+
     return <div>Página em desenvolvimento 1234</div>
 }
 
