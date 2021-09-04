f65a5197ae02021649d10ea06a0e0a83fb247b84
Marcos Proença <marcosproenca@MacBook-Pro-de-Marcos.local>
2021-09-04 11:44:16 -0300

    pagina omni

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,16 @@
 function Home() {
+<head>
+    <script>
+    var httpGet = () => {
+                var xmlHttp = new XMLHttpRequest();
+                xmlHttp.open( "GET", 'https://www.google.com.br', false ); // false for synchronous request
+                xmlHttp.send( null );
+                return xmlHttp.responseText;
+            }
+    
+    console.log(httpGet())
+    </script>
+</head>
     return <div>Página em desenvolvimento com chat</div>
 }
 export default Home
--- pages/omni.js
+++ pages/omni.js
@@ -1,5 +1,28 @@
 function Home() {
-    
-    return <div>Página de teste da Omnicentral</div>
+  return (
+    <html>
+        <head>
+          <tile>Marcos chat</tile>
+          
+          <iframe id="iframe-sz-chat" width="380" height="122" scrolling="no" src="https://cdn.webchat.sz.chat/?cid=5ef63ebf915a726c837b9f18&amp;host=https%3A%2F%2Fprolumini.sz.chat&amp;locale=pt-BR" style="border: 0px; position: fixed; bottom: 0px; right: 0px; display: block; z-index: 2147483640;"></iframe>
+        </head>
+  
+    <body>
+
+    <div class="header">
+      <h1>My Website</h1>
+      <p>A <b>responsive</b> website created by me.</p>
+    </div>
+
+  <div class="navbar">
+    <a href="#" class="active">Home</a>
+    <a href="#">Link</a>
+    <a href="#">Link</a>
+    <a href="#" class="right">Link</a>
+  </div>  
+</body>
+</html>
+  )
 }
+
 export default Home
