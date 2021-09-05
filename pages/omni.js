898b1aeee827c71b4968ce4e2b167927773b4155
Marcos Proença <marcosproenca@MacBook-Pro-de-Marcos.local>
2021-09-05 18:56:06 -0300

    WIP

--- pages/omni.js
+++ pages/omni.js
@@ -1,28 +1,5 @@
 function Home() {
-  return (
-    <html>
-        <head>
-          <tile>Marcos chat</tile>
-          
-          <iframe id="iframe-sz-chat" width="380" height="122" scrolling="no" src="https://cdn.webchat.sz.chat/?cid=5ef63ebf915a726c837b9f18&amp;host=https%3A%2F%2Fprolumini.sz.chat&amp;locale=pt-BR" style="border: 0px; position: fixed; bottom: 0px; right: 0px; display: block; z-index: 2147483640;"></iframe>
-        </head>
-  
-    <body>
-
-    <div class="header">
-      <h1>My Website</h1>
-      <p>A <b>responsive</b> website created by me.</p>
-    </div>
-
-  <div class="navbar">
-    <a href="#" class="active">Home</a>
-    <a href="#">Link</a>
-    <a href="#">Link</a>
-    <a href="#" class="right">Link</a>
-  </div>  
-</body>
-</html>
-  )
+  <h1>Omni page</h1>
 }
 
 export default Home
