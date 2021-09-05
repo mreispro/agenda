656ad35bd401cc347ca34e7204027cc9f51b5459
Marcos Proença <marcosproenca@MacBook-Pro-de-Marcos.local>
2021-09-05 18:44:22 -0300

    WIP

--- pages/index.js
+++ pages/index.js
@@ -1,15 +1,4 @@
 function Home() {
-<head>
-    <script type="text/javascript">
-        function myFunction() {
-            var xmlHttp = new XMLHttpRequest();
-            xmlHttp.open( "GET", 'https://www.google.com.br', false ); // false for synchronous request
-            xmlHttp.send( null );
-            document.getElementById("#demo").innerHTML = xmlHttp.responseText;
-        }
-        myFunction();
-    </script>
-</head>
     return <div id="demo" >Página em desenvolvimento com script 2</div>
 }
 export default Home
