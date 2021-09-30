d2995bc1030c703e4694bb6896e38dc848f74d1c
Marcos Proença <marcosproenca@MacBook-Pro-de-Marcos.local>
2021-09-30 11:41:06 -0300

    WIP

--- pages/index.js
+++ pages/index.js
@@ -1,25 +1,23 @@
-function Home() {
-    
-return (
-<html>
-  <head>
+export const Home = () => (
+  <>
     <title> Imersão Dev Alura - Aula 01</title>
-  </head>
-  <body>
-    <div class="container">
-      <h1 class="page-title">
-      Calculadora de média</h1>
-      <p class="page-subtitle">
-      Descubra qual a sua média de nota de todas as provas!
+    <div className="container">
+      <h1 className="page-title">Calculadora de média</h1>
+      <p className="page-subtitle">
+        Descubra qual a sua média de nota de todas as provas!
       </p>
-      <img src="https://www.alura.com.br/assets/img/imersoes/dev-2021/logo-imersao-conversor-de-moedas.svg" class="page-logo" alt="" /> 
+      <img
+        src="https://www.alura.com.br/assets/img/imersoes/dev-2021/logo-imersao-conversor-de-moedas.svg"
+        className="page-logo"
+        alt
+      />
     </div>
-    <a href="https://alura.com.br/" target="_blank"><img src="https://www.alura.com.br/assets/img/home/alura-logo.svg" alt="" class="alura-logo" />
+    <a href="https://alura.com.br/" target="_blank">
+      <img
+        src="https://www.alura.com.br/assets/img/home/alura-logo.svg"
+        alt
+        className="alura-logo"
+      />
     </a>
-  </body>
-</html>
+  </>
 )
-
-}
-   
-export default Home
