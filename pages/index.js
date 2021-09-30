027707dacd292cb61070ca970f39692fc7f19f34
Marcos Proença <marcosproenca@MacBook-Pro-de-Marcos.local>
2021-09-30 11:20:04 -0300

    WIP

--- pages/index.js
+++ pages/index.js
@@ -12,9 +12,9 @@ return (
       <p class="page-subtitle">
       Descubra qual a sua média de nota de todas as provas!
       </p>
-      <img src="https://www.alura.com.br/assets/img/imersoes/dev-2021/logo-imersao-conversor-de-moedas.svg" class="page-logo" alt="">
+      <img src="https://www.alura.com.br/assets/img/imersoes/dev-2021/logo-imersao-conversor-de-moedas.svg" class="page-logo" alt="" /> 
     </div>
-    <a href="https://alura.com.br/" target="_blank"><img src="https://www.alura.com.br/assets/img/home/alura-logo.svg" alt="" class="alura-logo">
+    <a href="https://alura.com.br/" target="_blank"><img src="https://www.alura.com.br/assets/img/home/alura-logo.svg" alt="" class="alura-logo" />
     </a>
   </body>
 </html>
