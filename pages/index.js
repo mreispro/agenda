bcba9e1014393bb23a1c2a2d233ea390561646be
Marcos Proença <marcosproenca@MacBook-Pro-de-Marcos.local>
2021-09-30 10:50:22 -0300

    WIP

--- pages/index.js
+++ pages/index.js
@@ -10,7 +10,11 @@ return (
            <div class="container">
                <h1 class="page-title">Primeira página</h1>
                <p class="page-subtitle"> primeiro texto da página</p>
+               <img src="https://www.alura.com.br/assets/img/imersoes/dev-2021/logo-imersao-conversor-de-moedas.svg" class="page-logo" alt=""></img>
            </div>
+           <a href="https://alura.com.br/" target="_blank">
+           <img src="https://www.alura.com.br/assets/img/home/alura-logo.svg" alt="" class="alura-logo">  
+           </a>
        </body>
    </html>
 )
