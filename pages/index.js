d68737413925ebdc313140299f0468893fddfa6b
Marcos Proença <marcosproenca@MacBook-Pro-de-Marcos.local>
2021-10-03 20:21:37 -0300

    WIP

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,5 @@
 function Home() {
-<<<<<<< HEAD
   return <div>Home 2</div>
 }
 
 export default Home
-=======
-  return <div> Home </div>
-}
-
-export default Home
->>>>>>> 096b4e95089d6b55218fdab3574cce0480294a89
